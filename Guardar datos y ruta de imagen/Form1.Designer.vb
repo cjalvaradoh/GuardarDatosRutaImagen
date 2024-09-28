9471b4f269db0c4c40fa4a20cef3173b512f16d1
cjalvaradoh <cjalvarado@gmail.com>
2024-09-28 13:27:52 -0600

    guardarDatos

---
+++ Guardar datos y ruta de imagen/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        PictureBox1 = New PictureBox()
+        Button2 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Century Gothic", 9.75F)
+        Label1.Location = New Point(121, 61)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(125, 17)
+        Label1.TabIndex = 0
+        Label1.Text = "Nombre Producto"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Century Gothic", 9.75F)
+        Label2.Location = New Point(121, 101)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(48, 17)
+        Label2.TabIndex = 1
+        Label2.Text = "Precio"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(254, 58)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(326, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(254, 101)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(326, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(366, 371)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "Guardar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(226, 199)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(364, 151)
+        PictureBox1.TabIndex = 5
+        PictureBox1.TabStop = False
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(283, 141)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(246, 23)
+        Button2.TabIndex = 6
+        Button2.Text = "Subir Imagen"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(PictureBox1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ Guardar datos y ruta de imagen/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private rutaImagen As String ' Variable para almacenar la ruta de la imagen
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Preparar ruta de carpeta
+        Dim rutaCarpeta As String = "C:\USPG24\"
+
+        ' Preparar ruta final y nombre de archivo
+        Dim rutaArchivo As String = Path.Combine(rutaCarpeta, "datos.txt")
+
+        ' Leer el contenido de los campos
+        Dim producto As String = TextBox1.Text
+        Dim precio As String = TextBox2.Text
+
+        ' Establecer/preparar fila que se almacenará en el documento de texto
+        Dim datos As String = $"Nombre Producto: {producto}, Precio: {precio}, Ruta Imagen: {rutaImagen}"
+
+        ' Insertar/agregar fila con datos en archivos de texto
+        Try
+            Using escritor As New StreamWriter(rutaArchivo, True)
+                escritor.WriteLine(datos)
+            End Using
+
+            ' Limpiar los TextBox después de guardar
+            TextBox1.Clear()
+            TextBox2.Clear()
+
+            MessageBox.Show("Información guardada", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show($"Error al guardar datos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim buscarFoto As New OpenFileDialog()
+
+        ' Nos permite abrir el diálogo para buscar un archivo
+        buscarFoto.Filter = "Archivos de Imagen | *.jpg;*.jpeg;*.png;*.gif;*.bmp|Todos los archivos|*.*"
+
+        If buscarFoto.ShowDialog() = DialogResult.OK Then
+            rutaImagen = buscarFoto.FileName ' Almacenar la ruta de la imagen
+            mostrarFoto(rutaImagen)
+        End If
+
+        ' Liberar recursos del OpenFileDialog
+        buscarFoto.Dispose()
+    End Sub
+
+    Private Sub mostrarFoto(ubicacionFoto As String)
+        Dim nuevaImagen As Image = Image.FromFile(ubicacionFoto)
+        PictureBox1.Image = nuevaImagen ' Inserción de la imagen
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom ' Amplía la imagen de la foto
+    End Sub
+End Class
